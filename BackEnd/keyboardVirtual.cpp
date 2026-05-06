38e8d97451e13625e560bebf0a87bade510fc053
Anthony <adiego8307@sdsu.edu>
2026-05-06 15:05:53 -0700

    KeyboardVirtual code

--- BackEnd/keyboardVirtual.cpp
+++ BackEnd/keyboardVirtual.cpp
@@ -6,11 +6,37 @@ class KEYBOARD{
 
     public:
         ///TODO: Handles raw inputs, such as movement, enter, backspace, space
-        void inputController (){
+        void inputController (WORD virtualKey){
+            INPUT input = { 0 };
+            input.type = INPUT_KEYBOARD;
+            input.ki.wVk = virtualKey;
 
-        }
+            // Press Key
+            SendInput(1, &input, sizeof(INPUT));
+
+            // Release Key
+            input.ki.dwFlags = KEYEVENTF_KEYUP;
+            SendInput(1, &input, sizeof(INPUT));
+    }
+
+
         ///TODO: handle converted CBC string to keyboard input
         void inputString(std::string input){
+            if (input.empty()) return;
 
+            for (char ch : input) {
+            INPUT inputs[2] = { 0 };
+
+            // Key Down (using Unicode)
+            inputs[0].type = INPUT_KEYBOARD;
+            inputs[0].ki.wScan = ch;
+            inputs[0].ki.dwFlags = KEYEVENTF_UNICODE;
+
+            // Key Up
+            inputs[1] = inputs[0];
+            inputs[1].ki.dwFlags |= KEYEVENTF_KEYUP;
+
+            SendInput(2, inputs, sizeof(INPUT));
         }
+    }
 };
