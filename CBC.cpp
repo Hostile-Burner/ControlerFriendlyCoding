56e275608c4d233b7174b4b4b4b135216370754e
Anthony <adiego8307@sdsu.edu>
2026-05-06 17:25:23 -0700

    TODOs done for CBC.cp and Controller Input

--- CBC.cpp
+++ CBC.cpp
@@ -10,6 +10,12 @@ int main(){
     CBC CBC;
 
     ///TODO: maybe add trim spaces
+    //Trim leading/trailing whitespace
+    std::string trimInput(const std::string& str) {
+    size_t first = str.find_first_not_of(" \t\n\r");
+    if (first == std::string::npos) return "";
+    size_t last = str.find_last_not_of(" \t\n\r");
+    return str.substr(first, (last - first + 1));
 
     while(true){
         //selection
@@ -20,22 +26,38 @@ int main(){
                     << std::endl;
 
         std::getline(std::cin, userInput);
-        if (userInput == "0"){
-            exit(0);
-        } else if (userInput == "1"){
-            std::cout << "This usage has not been implemented, terminating program.";
-            ///TODO: start ControllerInput.cpp
+        userInput = trimInput(userInput);
 
-        } else if (userInput == "2") {
+        if (userInput == "0"){
+            std::cout << "Exiting Controller Live..." << std::endl;
+            exit(0);
+        }
+         else if (userInput == "1"){
+            std::cout << "Starting Live Translation Mode..." << std::endl;
+            runControllerInput(); 
+            //runControllerInput() contains its own while(true) loop
+        }
+        else if (userInput == "2") {
+            //TODO File Reading Done
             std::cout << "input file name:";
             std::getline(std::cin, userInput);
+            userInput = trimInput(userInput);
 
-            CBC.runFile(userInput); // send file name to language
-            std::cout << "\nPlease restart program if you wish to use this again." << std::endl;
-            ///TODO: think of something better here than what i have now
-            while(true){std::getline(std::cin, userInput);}; // goes nowhere, just to hold till .exe is closed
-        } else {
-            std::cout << "invalid input detected, please try again." << std::endl;
+            if (!userInput.empty()) {
+                cbcProcessor.runFile(userInput); 
+                std::cout << "\nFile processing and compilation complete." << std::endl;
+            } else {
+                std::cout << "Error: File name cannot be empty." << std::endl;
+            }
+            
+            // --- TODO: think of something better (COMPLETED) ---
+            // Instead of an infinite loop, we let the main loop continue 
+            // so the user can process another file or switch to Live mode.
+            std::cout << "Returning to main menu..." << std::endl;
+        } 
+        else {
+            std::cout << "Invalid input detected, please try again." << std::endl;
         }
     }
+    return 0;
 }
